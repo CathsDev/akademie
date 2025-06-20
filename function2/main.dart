984a8092a8a8a3054e5dc240a3d4e5315826ba42
c.sauer <caths.dev@gmail.com>
2025-06-20 16:04:40 +0200

    aufgabe 2

--- function2/main.dart
+++ function2/main.dart
@@ -1,9 +1,21 @@
 void main() {
-  print(sum(1, 2, 3));
-  print(sum(12, 2, 33));
-  print(sum(81, 22, 53));
+  // print(sum(1, 2, 3));
+  // print(sum(12, 2, 33));
+  // print(sum(81, 22, 53));
+
+  print(countLength("Hallo Welt!"));
+  print(countLength("Erstelle eine Funktion"));
+  print(
+    countLength(
+      "Erstelle eine Funktion, die einen Text als Parameter entgegennimmt und die Anzahl der Zeichen dieses Textes zurückgibt",
+    ),
+  );
 }
 
 int sum(int x, int y, int z) {
   return x + y + z;
+}
+
+int countLength(String txt) {
+  return txt.length;
 }
