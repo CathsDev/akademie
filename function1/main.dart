af07295c3dbadb9e280b42e8c43a1b94477ed9d3
c.sauer <caths.dev@gmail.com>
2025-06-20 15:39:57 +0200

    aufgabe2

--- function1/main.dart
+++ function1/main.dart
@@ -1,9 +1,19 @@
 void main() {
-  printNumber(5);
-  printNumber(8);
-  printNumber(-3);
+  // printNumber(5);
+  // printNumber(8);
+  // printNumber(-3);
+
+  triplePrint("Hallo");
+  triplePrint("World");
+  triplePrint("Hallo Welt!");
 }
 
 void printNumber(int x) {
   print(x);
+}
+
+void triplePrint(String txt) {
+  for (int i = 0; i < 3; i++) {
+    print(txt);
+  }
 }
