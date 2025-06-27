761872103e022cd036ea9115d1c52c10c3cd3729
c.sauer <caths.dev@gmail.com>
2025-06-27 14:18:14 +0200

    keyGenerator

--- konsolenprogramm/main.dart
+++ konsolenprogramm/main.dart
@@ -1,5 +1,35 @@
-class KeyGenerator {}
+import 'dart:math';
+import 'dart:io';
+
+class KeyGenerator {
+  int keyLength;
+
+  KeyGenerator({required this.keyLength});
+
+  String generateKey() {
+    const keyChars =
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+    String chars = keyChars;
+
+    final rand = Random.secure();
+    return List.generate(
+      keyLength,
+      (index) => chars[rand.nextInt(chars.length)],
+    ).join();
+  }
+}
 
 void main() {
-  List.generate(26, (index) => print(String.fromCharCode(index + 65)));
+  stdout.write('Länge: ');
+  int? keyLength = int.tryParse(stdin.readLineSync() ?? '');
+  if (keyLength == null || keyLength <= 0) {
+    print('Ungültige Eingabe!');
+    return;
+  }
+
+  var generator = KeyGenerator(keyLength: keyLength);
+
+  String key = generator.generateKey();
+  print('Key: $key');
 }
