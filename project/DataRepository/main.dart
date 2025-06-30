9cebe26e5cf25127420acddf97e90ca4784813d8
c.sauer <caths.dev@gmail.com>
2025-06-30 11:41:47 +0200

    add MockDataRepository

--- project/DataRepository/main.dart
+++ project/DataRepository/main.dart
@@ -1,10 +1,37 @@
 class Data {}
 
-abstract class DataRepository {
+abstract class DatabaseRepository {
   void sendData(Data data); // create
   List<Data> getData(); // read
   List<Data> updateData(); // update
   List<Data> deleteData(); // delete
+}
+
+class MockDataRepository implements DatabaseRepository {
+  List<String> data = [];
+
+  @override
+  void sendData(Data data) {
+    // TODO: implement sendData
+  }
+
+  @override
+  List<Data> getData() {
+    // TODO: implement getData
+    throw UnimplementedError();
+  }
+
+  @override
+  List<Data> updateData() {
+    // TODO: implement updateData
+    throw UnimplementedError();
+  }
+
+  @override
+  List<Data> deleteData() {
+    // TODO: implement deleteData
+    throw UnimplementedError();
+  }
 }
 
 void main() {}
