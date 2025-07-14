fe716bcf1c9fc0c4cbe2044da165e5f1fc4ec900
c.sauer <caths.dev@gmail.com>
2025-07-14 12:59:48 +0200

    changes

--- test_project/flutter_widget_app/lib/main.dart
+++ test_project/flutter_widget_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widget_app/boxes_row.dart';
+/* import 'package:flutter_widget_app/boxes_row.dart';
 import 'package:flutter_widget_app/headline_padding.dart';
-import 'package:flutter_widget_app/icons_row.dart';
+import 'package:flutter_widget_app/icons_row.dart'; */
 
 void main() {
   runApp(const MainApp());
@@ -11,6 +11,11 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
+  Widget build(BuildContext context) {
+    return MyListViewWidget();
+  }
+
+  /*  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -34,5 +39,67 @@ class MainApp extends StatelessWidget {
         ),
       ),
     );
+  } */
+}
+
+class MyListViewWidget extends StatelessWidget {
+  MyListViewWidget({super.key});
+
+  final List<Product> products = [
+    Product('Stift', 1.99),
+    Product('Milch', 1.49),
+    Product('Block', 3.99),
+    Product('Buch', 9.99),
+    Product('Brot', 2.89),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Liste')),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: products.length,
+          itemBuilder: (BuildContext context, int index) {
+            final product = products[index];
+            return Container(
+              height: 50,
+              child: Center(
+                child: ListTile(
+                  title: Text(
+                    'Bezeichnung: ${product.name}, Preis: ${product.price.toStringAsFixed(2)}€',
+                  ),
+                  trailing: Icon(Icons.shopping_cart),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
+
+  /* @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return ListTile(
+            title: Text(product.name),
+            subtitle: Text(product.price.toStringAsFixed(2)),
+            leading: Icon(Icons.shopping_cart),
+          );
+        },
+      ),
+    );
+  } */
+}
+
+class Product {
+  String name;
+  double price;
+  Product(this.name, this.price);
 }
